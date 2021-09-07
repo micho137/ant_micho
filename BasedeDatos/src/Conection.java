7d0442b9e0803f116dd1cb4fa3506b0b468621af
TheSebasx11 <sebabalcar18@gmail.com>
2021-09-07 00:40:05 -0500

    Cagué unas cosas y las arreglé

--- BasedeDatos/src/Conection.java
+++ BasedeDatos/src/Conection.java
@@ -9,7 +9,7 @@ public class Conection {
     private Connection conexion = null;
     private ResultSet rs = null;
     private Statement s = null;
-<<<<<<< Updated upstream
+
 
     public Connection getConexion() {
         return conexion;
@@ -19,22 +19,11 @@ public class Conection {
         /* if (conexion != null) {
             return;
         }*/
-=======
-    
-    
-    public boolean conexion() {
-        //  if (conexion != null) {
-        //          return;
-        //    }
->>>>>>> Stashed changes
         String host = "localhost";
         String DB = "Register";
         String user = "postgres";
         String pass = "Micho137";
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
         try {
             conexion = DriverManager.getConnection("jdbc:postgresql://" + host + ":5432/" + DB, user, pass);
             return true;
@@ -44,7 +33,7 @@ public class Conection {
             return false;
         }
     }
-<<<<<<< Updated upstream
+
 
     public void Registro(String username, String pass) {
         try {
@@ -53,21 +42,8 @@ public class Conection {
                     + "\nVALUES ('" + username + "', '" + pass + "')");
             if (z == 1) {
             } else {
-=======
-    
-    
 
-    public void Registro(String username, String pass) {
 
-        try {
-            s = conexion.createStatement();
-            int z = s.executeUpdate("INSERT INTO \"NewStudent\"(\"Username\", \"Pass\")"
-                    + "\nVALUES ('" + username + "', '" + pass + "')");
-            if (z == 1) {
-
-            } else {
-
->>>>>>> Stashed changes
                 JOptionPane.showMessageDialog(null, "Error");
             }
             JOptionPane.showMessageDialog(null, "Estudiante registrado exitosamente");
@@ -80,19 +56,15 @@ public class Conection {
 
         try {
             s = conexion.createStatement();
-<<<<<<< Updated upstream
+
             int z = s.executeUpdate("INSERT INTO \"Teacher\"(\"Username\", \"Password\")"
                     + "\nVALUES ('" + username + "', '" + pass + "')");
             if (z == 1) {
             } else {
-=======
-            int z = s.executeUpdate("INSERT INTO \"NewDoc\"(\"Username\", \"Password\")"
-                    + "\nVALUES ('" + username + "', '" + pass + "')");
-            if (z == 1) {
 
-            } else {
+        
 
->>>>>>> Stashed changes
+
                 JOptionPane.showMessageDialog(null, "Error");
             }
             JOptionPane.showMessageDialog(null, "Docente registrado exitosamente");
@@ -101,11 +73,9 @@ public class Conection {
         }
     }
 
-<<<<<<< Updated upstream
+
     public void mostrarEstudiantes(DefaultTableModel tab) {
-=======
-    public void mostrarEstudiantes(JTable tab1, DefaultTableModel tab) {
->>>>>>> Stashed changes
+
 
         try {
             s = conexion.createStatement();
